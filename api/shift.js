280fb6ba115e18739929b03aa566b3cf29d70b29
Anuja-Ranganathan <anujaranganathanDeveloper@gmail.com>
2026-01-25 23:40:17 +0530

    fdndssdsdddsss

--- api/shift.js
+++ api/shift.js
@@ -25,8 +25,12 @@ router.post(
     const arr = [];
     try {
 
-      const normalizedDate = new Date(req.body.date);
-      console.log(req.body.date);
+      var normalizedDate = new Date(req.body.date);
+
+     normalizedDate= normalizedDate.getTime() + ( 5.5 * 60 * 60 * 1000 )
+      
+      console.log(normalizedDate);
+
       const check = await ShiftPlan.findOne({ date: normalizedDate, shift: req.body.shift });
       console.log(check);
       if (check !== null) {
@@ -106,11 +110,10 @@ router.get(
   catchAsyncErrors(async (req, res, next) => {
     try {
 
-      const now = new Date(req.query.date).setHours(0, 0, 0, 0);
-const utc = new Date(now).toUTCString();
-console.log(utc.toString()); // ignore the timezone
+console.log(new Date(new Date(req.query.date).setHours(0, 0, 0, 0)+( 5.5 * 60 * 60 * 1000 )));
+ // ignore the timezone
       const shift = await ShiftPlan.find({
-        date: { $eq: new Date(new Date(utc)) },
+        date: { $eq: new Date(new Date(req.query.date).setHours(0, 0, 0, 0)+( 5.5 * 60 * 60 * 1000 )) },
       }).populate({
         path: 'plan',
         populate: [{ path: 'employee', model: 'Employee' }, { path: 'machine', model: 'Machine' }]
